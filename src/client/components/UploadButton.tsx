83b6eb0973d592c079c483da8f8f32d6502a81c8
adreyponamarev <andrey.ponamarev@maxymiser.com>
2019-01-21 23:26:40 +0100

    Move upload logic from entry point to UploadButton component

--- src/client/components/UploadButton.tsx
+++ src/client/components/UploadButton.tsx
@@ -2,6 +2,23 @@ require('./UploadButton.less')
 import * as React from 'react'
 
 export class UploadButton extends React.Component<{}, {}> {
+    private _fileInputRef = React.createRef<HTMLInputElement>()
+    private _handlerSubmit = async event => {
+        event.preventDefault()
+        const filesField: HTMLInputElement = this._fileInputRef.current
+        const formData = new FormData()
+        for (let i = 0; i < filesField.files.length; i += 1) {
+            const file = filesField.files[i]
+            const fileName = file.name
+            formData.append('uploads', file, fileName)
+        }
+        const uploadResult = await fetch('/api/v1/upload', {
+            method: 'POST',
+            body: formData
+        })
+        const uploadResultJSON = await uploadResult.json()
+        console.log(`uploadResultJSON`, uploadResultJSON)
+    }
     private _handleChange = event => {
         event.preventDefault()
         console.log(`Selected files: ${event.target.files}`)
@@ -9,10 +26,10 @@ export class UploadButton extends React.Component<{}, {}> {
 
     render() {
         return (
-            <label className='UploadButtonLabel'>
-                Upload file
-                <input className='UploadButtonInput' type='file' onChange={this._handleChange} />
-            </label>
+            <form onSubmit={this._handlerSubmit}>
+                <input ref={this._fileInputRef} type='file' multiple />
+                <button type='submit'>Submit</button>
+            </form>
         )
     }
 }
