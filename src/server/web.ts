85337c794cab303d0f2ccf8884ba37ffafc04aaa
Dmitry Podgorniy <podgorniy.d@gmail.com>
2019-02-03 00:45:23 +0100

    In dev env create default user "user" with password "123"

--- src/server/web.ts
+++ src/server/web.ts
@@ -4,6 +4,21 @@ import {initRoutes} from './routes'
 import * as path from 'path'
 import {connect} from 'mongoose'
 import {MONGO_URL, WEB_PORT} from './env'
+import {hashString} from './utils'
+import {UserModel} from './models/user'
+import {isDev} from '../common/lib'
+
+async function createDefaultUser() {
+    const DEFAULT_USERNAME = 'user'
+    const DEFAULT_PASSWORD = '123'
+    const defaultUser = await UserModel.findOne({name: DEFAULT_USERNAME})
+    if (!defaultUser) {
+        return new UserModel({
+            name: DEFAULT_USERNAME,
+            password: await hashString(DEFAULT_PASSWORD)
+        }).save()
+    }
+}
 
 export async function startServer() {
     const app = express()
@@ -14,6 +29,10 @@ export async function startServer() {
         // @ts-ignore
         {useNewUrlParser: true}
     )
+    // Create user
+    if (isDev()) {
+        await createDefaultUser()
+    }
     initMiddleware(app)
     initRoutes(app)
     app.listen(WEB_PORT)
