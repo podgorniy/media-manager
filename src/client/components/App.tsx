dc9365e97a6e82e9345b395eaa5aad69669601c8
Dmitry Podgorniy <podgorniy.d@gmail.com>
2019-02-09 21:05:31 +0100

    Expose app state to global var a when in development

--- src/client/components/App.tsx
+++ src/client/components/App.tsx
@@ -3,8 +3,12 @@ import {AppState} from '../app-state'
 import {Provider} from 'mobx-react'
 import {Navigation} from './Navigation'
 import {MainView} from './MainView'
+import { isDev } from '../../common/lib'
 
 const appState = new AppState(window['initialState'])
+if (isDev()) {
+    window['a'] = appState
+}
 
 export class App extends React.Component<{}, {}> {
     render() {
